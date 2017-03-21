e268916c717dcb328bd4182a70737aa092c43d76
John Slota <john.slota@gmail.com>
2017-03-20 20:25:38 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var React = require('react');
+var ReactNative = require('react-native');
+
+var styles = ReactNative.StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+class PropertyFinderApp extends React.Component {
+  render() {
+    return React.createElement(ReactNative.Text, {style: styles.text}, "Hello World!");
+  }
+}
+
+ReactNative.AppRegistry.registerComponent('PropertyFinder', function() { return PropertyFinderApp });
